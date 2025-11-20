7862b9803ea2c0671f14f7db7c0ed0d26d82b26a
ganstotor <ganstotor@gmail.com>
2025-11-20 23:31:51 +0200

    feat: add axios

--- src/pages/Menu/Menu.tsx
+++ src/pages/Menu/Menu.tsx
@@ -1,23 +1,51 @@
+import { useEffect, useState } from "react";
 import Headling from "../../components/Headling/Headling";
-import ProductCard from "../../components/ProductCard/ProductCard";
 import Search from "../../components/Search/Search";
+import { PREFIX } from "../../helpers/API";
+import type { Product } from "../../interfaces/product.interface";
 import styles from './Menu.module.css';
+import axios, { AxiosError } from "axios";
+import { MenuList } from "./MenuList/MenuList";
 
 export function Menu() {
+    const [products, setProducts] = useState<Product[]>([]);
+    const [isLoading, setIsLoading] = useState<boolean>(false);
+    const [error, setError] = useState<string | undefined> ();
+
+    const getMenu = async () => {
+        try {
+            setIsLoading(true);
+            await new Promise<void>((resolve) => {
+                setTimeout(() => {
+                    resolve();
+                }, 2000)
+            })
+            const {data} = await axios.get<Product[]>(`${PREFIX}/products`)
+            setProducts(data)
+            setIsLoading(false);
+        } catch (e) {
+            console.error(e);
+            if (e instanceof AxiosError) {
+                setError(e.message);
+            }
+            setIsLoading(false);
+            return;
+        }
+    };
+
+    useEffect(() => {
+        getMenu();
+    }, []);
+
     return <>
         <div className={styles['head']}>
             <Headling>Меню</Headling>
             <Search placeholder='Введите блюдо или состав' />
         </div>
         <div>
-            <ProductCard
-                id={1}
-                title='Наслаждние'
-                description="Салями, руккола, помидоры, оливки"
-                rating={4.5}
-                price={300}
-                image="/pizza-1.jpg"
-            />
+            {error && <>{error}</>}
+            {!isLoading && <MenuList products={products} />}
+            {isLoading && <>Загружаем продукты...</>}
         </div>
     </>
 }
