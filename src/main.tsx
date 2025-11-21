0627fcddfedd71fafcb82e36c254772da1dd5056
ganstotor <ganstotor@gmail.com>
2025-11-21 22:38:01 +0200

    feat: add loader

--- src/main.tsx
+++ src/main.tsx
@@ -1,12 +1,15 @@
-import React from 'react';
+import React, { Suspense, lazy } from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import { RouterProvider, createBrowserRouter } from 'react-router-dom';
-import { Menu } from "./pages/Menu/Menu.tsx";
 import { Cart } from "./pages/Cart/Cart.tsx";
-import { Error } from "./pages/Error/Error.tsx";
+import { Error as ErrorPage } from "./pages/Error/Error.tsx";
 import { Layout } from './layout/Menu/Layout.tsx';
 import { Product } from './pages/Product/Product.tsx';
+import { PREFIX } from './helpers/API.ts';
+import axios from 'axios';
+
+const Menu = lazy(() => import('./pages/Menu/Menu'));
 
 const router = createBrowserRouter([
   {
@@ -15,7 +18,7 @@ const router = createBrowserRouter([
     children: [
       {
         path: '/',
-        element: <Menu />
+        element: <Suspense fallback={<>Загрузка...</>}><Menu /></Suspense>
       },
       {
         path: '/cart',
@@ -23,13 +26,23 @@ const router = createBrowserRouter([
       },
       {
         path: '/product/:id',
-        element: <Product />
+        element: <Product />,
+        errorElement: <>Ошибочка</>,
+        loader: async ({params}) => {
+          await new Promise<void>((resolve) => {
+            setTimeout(() => {
+                resolve();
+            }, 2000)
+        })
+          const {data} = await axios.get(`${PREFIX}/products/${params.id}`);
+          return data;
+        }
       },
     ]
   },
   {
     path: '/*',
-    element: <Error />
+    element: <ErrorPage />
   }
 ]);
 
