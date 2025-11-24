609313d79d80122e41d847f5b653e792e089e435
ganstotor <ganstotor@gmail.com>
2025-11-24 23:37:35 +0200

    feat: add checkout and finish project

--- src/pages/Cart/Cart.tsx
+++ src/pages/Cart/Cart.tsx
@@ -1,18 +1,25 @@
-import { useSelector } from "react-redux";
+import { useDispatch, useSelector } from "react-redux";
 import Headling from "../../components/Headling/Headling";
-import type { RooState } from "../../store/store";
+import {type AppDispatch, type RooState } from "../../store/store";
 import CardItem from "../../components/CartItem/CartItem";
 import { useEffect, useState } from "react";
 import type { Product } from "../../interfaces/product.interface";
 import axios from "axios";
 import { PREFIX } from "../../helpers/API";
 import styles from './Cart.module.css'
+import Button from "../../components/Button/Button";
+import { useNavigate } from "react-router-dom";
+import { cartActions } from "../../store/cart.slice";
 
 const DELIVERY_FEE = 169;
 
 export function Cart() {
     const [cartProducts, setCartProducts] = useState<Product[]>([]);
     const items = useSelector((s: RooState) => s.cart.items);
+    const jwt = useSelector((s: RooState) => s.user.jwt);
+    const dispatch = useDispatch<AppDispatch>()
+    const navigate = useNavigate()
+
     const total = items.map(i => {
         const product = cartProducts.find(p => p.id === i.id);
         if (!product) {
@@ -29,6 +36,18 @@ export function Cart() {
     const loadAllItems = async () => {
         const res = await Promise.all(items.map(i => getItem(i.id)));
         setCartProducts(res);
+    };
+
+    const checkout = async () => {
+        await axios.post(`${PREFIX}/order`, {
+            products: items
+        }, {
+            headers: {
+                Authorization: `Bearer ${jwt}`
+            }
+        });
+        dispatch(cartActions.clean())
+        navigate('/success')
     }
 
     useEffect(() => {
@@ -55,10 +74,12 @@ export function Cart() {
         </div>
         <hr className={styles['hr']} />
         <div className={styles['line']}>
-            <div className={styles['text']}>Итог {items.length}</div>
+            <div className={styles['text']}>Итог <span className={styles['total-count']}>({items.length})</span></div>
             <div className={styles['price']}>{total + DELIVERY_FEE}&nbsp;<span>&#8372;</span></div>
         </div>
-        <hr className={styles['hr']} />
+        <div className={styles['checkout']}>
+            <Button appearance="big" onClick={checkout}>оформить</Button>
+        </div>
     </>
 
 }
---
+++ src/pages/Success/Success.tsx
@@ -0,0 +1,14 @@
+import { useNavigate } from "react-router-dom";
+import Button from "../../components/Button/Button";
+import styles from './Success.module.css'
+
+export function Success() {
+  const navigate = useNavigate();
+  return (
+    <div className={styles['success']}>
+      <img src="/pizza.jfif" alt="" />
+      <div className={styles['text']}>Ваш заказ успешно оформлен!</div>
+      <Button appearance="big" onClick={() => navigate('/')}>сделать новый</Button>
+    </div>
+  )
+}
